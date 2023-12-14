0ee33f88a6113c6dfbdd1076dc20134d8c3a786c
Davis <daviesnyakiba@gmail.com>
2023-12-14 19:04:06 +0300

    Completed Assignment

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,10 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt","w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt","a") as file:
+        file.write(f"\n{append_content}")
 
 def read_file(file_name):
     pass
